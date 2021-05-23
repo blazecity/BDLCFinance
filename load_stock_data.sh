7ac1a6ae5aa4b4c820a6d514c249db5359ef6085
darkshine0x <jan.baumann@stud.hslu.ch>
2021-05-23 17:28:28 +0000

    changed python to python3

--- load_stock_data.sh
+++ load_stock_data.sh
@@ -4,9 +4,9 @@ jq -c '.[] | .ticker' $2 | while read i; do
     SYMBOL=$(echo $i | tr -d '\r"')
     echo "Current stock " ${SYMBOL}
     if [ "$1" == "-d" ]; 
-        then python bdlcfinance.py ${1:-d} ${SYMBOL} | jq -c '.data[0] | with_entries(.key |= ascii_downcase) | with_entries(if .key == "stock splits" then .key = "stockSplits" else . end)' >> data/yfinance_price_history.json
+        then python3 bdlcfinance.py ${1:-d} ${SYMBOL} | jq -c '.data[0] | with_entries(.key |= ascii_downcase) | with_entries(if .key == "stock splits" then .key = "stockSplits" else . end)' >> data/yfinance_price_history.json
         else if [ "$1" == "-m" ]
-        then python bdlcfinance.py ${1:-d} ${SYMBOL} | jq -c '.data[] | with_entries(.key |= ascii_downcase) | with_entries(if .key == "stock splits" then .key = "stockSplits" else . end)' >> data/yfinance_price_history.json
+        then python3 bdlcfinance.py ${1:-d} ${SYMBOL} | jq -c '.data[] | with_entries(.key |= ascii_downcase) | with_entries(if .key == "stock splits" then .key = "stockSplits" else . end)' >> data/yfinance_price_history.json
         fi
     fi
 done
