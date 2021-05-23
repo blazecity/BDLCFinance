646f40421a13c163a2dac0c44d7e3106b7cd3fa4
jbaumann2 <jan.baumann2@outlook.com>
2021-05-23 16:37:37 +0200

    added echo for current stock

--- load_stock_data.sh
+++ load_stock_data.sh
@@ -2,5 +2,6 @@
 
 jq -c '.[] | .ticker' $1 | while read i; do
     SYMBOL=$(echo $i | tr -d '\r"')
+    echo "Current stock " ${SYMBOL}
     python bdlcfinance.py ${SYMBOL} | jq -c '.data[] | with_entries(.key |= ascii_downcase) | with_entries(if .key == "stock splits" then .key = "stockSplits" else . end)' >> data/yfinance_price_history.json
 done
