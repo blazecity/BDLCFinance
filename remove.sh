2c49ff75faf4e8b3ff5bd3599bf406f74990d525
jbaumann2 <jan.baumann2@outlook.com>
2021-05-23 21:08:37 +0200

    changed file path

--- remove.sh
+++ remove.sh
@@ -7,7 +7,7 @@ hive_cli -f drop.sql
 
 hdfs dfs -rm /user/${USER}/load/yfinance_price_history/yfinance_price_history.json
 
-hdfs dfs -ls -R /user/${USER}/load/yfinance_price_history
+hdfs dfs -ls -R /user/${USER}/load/yfinance_price_history/
 
 hive_cli <<EOSQL
   SHOW DATABASES;
