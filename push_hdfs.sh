d748382c02b429b14b0cd1f456792d073376c6a0
jbaumann2 <jan.baumann2@outlook.com>
2021-05-23 21:07:26 +0200

    changed path to files

--- push_hdfs.sh
+++ push_hdfs.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-hdfs dfs -mkdir /user/${USER}/load/yfinance_price_history
-hdfs dfs -ls /user/${USER}/load/yfinance_price_history
-hdfs dfs -put -f data/yfinance_price_history.json /user/${USER}/load/yfinance_price_history
-hdfs dfs -ls /user/${USER}/load/yfinance_price_history
+hdfs dfs -mkdir /user/${USER}/load/yfinance_price_history/
+hdfs dfs -ls /user/${USER}/load/yfinance_price_history/
+hdfs dfs -put -f data/yfinance_price_history.json /user/${USER}/load/yfinance_price_history/
+hdfs dfs -ls /user/${USER}/load/yfinance_price_history/
